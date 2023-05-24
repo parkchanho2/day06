97583eef75de793f612ec4b1855686dea6231ad4
chanho <codkdls11111@naver.com>
2023-05-24 21:24:02 +0900

    day06

---
+++ src/string/Quiz02.java
@@ -0,0 +1,33 @@
+package string;
+
+import java.util.ArrayList;
+
+public class Quiz02 {
+	  public static void main(String [] args){
+//	      String str = new String("Have a nice day Have a nice day Have a nice day");
+//	      int i=0;
+//	      while(i < str.length()) {
+//	         if(str.charAt(i) == 'a')
+//	            System.out.print(i+", ");
+//	         i++;
+//	      }
+		  
+//		  String str = new String("It is a fun java programming");
+//		  int i = 0;
+//		  int a = 0, g = 0, c = 0;
+//		  while(i < str.length()) {
+//			  if(str.charAt(i) == 'a') 
+//				  a++;
+//			  else if(str.charAt(i) == 'g') 
+//				  g++;
+//			  i++;
+//		  }
+//			  System.out.println("총 개수: " + str.length());
+//			  System.out.println("a 개수: " + a);
+//			  System.out.println("g 개수: " + g);
+	
+		  String str = new String("tESt sTring    change    first");
+		  String changeStr = new String();
+	  
+	  }
+}
