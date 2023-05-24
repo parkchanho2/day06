3fe82714745090e5136d438f1752e6b211bcd8e2
chanho <codkdls11111@naver.com>
2023-05-24 21:49:36 +0900

    day06

--- src/string/Quiz02.java
+++ src/string/Quiz02.java
@@ -26,8 +26,37 @@ public class Quiz02 {
 //			  System.out.println("a 개수: " + a);
 //			  System.out.println("g 개수: " + g);
 	
-		  String str = new String("tESt sTring    change    first");
-		  String changeStr = new String();
-	  
+		  String str = new String("tESt  sTring  change   first");
+			String changeStr = new String();
+			int i=0;
+
+			str = str.toLowerCase();
+			if(str.charAt(0) >='a' && str.charAt(0) <='z') {
+				changeStr+=(char)(str.charAt(i)-32);
+				i++;
+			}
+			while(i<str.length()) {
+				if(str.charAt(i) != ' ') {
+					changeStr+=str.charAt(i);
+					i++;
+					continue;
+				}
+				while(str.charAt(i) == ' ') {
+					changeStr+=str.charAt(i);
+					i++;
+				}
+				if(str.charAt(i) >= 'a' && str.charAt(i) <='z')
+					changeStr+=(char)(str.charAt(i)-32);
+				i++;
+			}
+			System.out.println(str);
+			System.out.println(changeStr);
+			System.out.println("종료");
+		}
+		  
+
+
 	  }
-}
+
+
+
